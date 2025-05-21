43769fcbfad82dee292255b8034b0d8816436fb1
Nestor <nestor_vmp@hotmail.com>
2025-05-21 20:06:31 +0200

    Proyecto completo: cors cambio

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ app.use(express.json());
 }));*/
 app.use(cors({
   origin: ['http://localhost:5173','https://pruebadesplieguebackend.onrender.com','http://192.168.1.136:3000','http://localhost:3000',
-    'https://prueba-despliegue-frontend.vercel.app','https://prueba-despliegue-frontend-3xiih9qlp-nestor-vmps-projects.vercel.app'],
+    'https://prueba-despliegue-frontend.vercel.app','https://prueba-despliegue-frontend-3xiih9qlp-nestor-vmps-projects.vercel.app',
+    'https://pruebadesplieguebackendseparado.onrender.com','https://prueba-despliegue-frontend.vercel.app/' ],
   credentials: true
 }));
 
